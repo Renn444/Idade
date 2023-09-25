baf038340b2d8fca50ee70c954a6dd1870025563
Renn444 <128076981+Renn444@users.noreply.github.com>
2023-09-25 17:50:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ def main():
         botao = st.button("Obtenha os valores")
         if botao: 
             resultados = [] 
+            for _ in range(Runs):
             # Definições das funções
             def fx(x): 
                 f = (beta/eta)*((x/eta)**(beta-1))*np.exp(-(x/eta)**beta) 
