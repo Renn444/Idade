1e8ba6b6769a8f89feb4741228cfea5681598ae1
Renn444 <128076981+Renn444@users.noreply.github.com>
2023-09-25 16:42:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random
+import math
 import numpy as np
 import pandas as pd
 from scipy.integrate import quad, dblquad
@@ -7,6 +7,8 @@ import streamlit as st
 import sys
 from streamlit import cli as stcli
 from PIL import Image
+
+# Funções e definições anteriores
 
 def main():
     col1, col2, col3 = st.columns(3)
@@ -36,6 +38,7 @@ def main():
         
         botao = st.button("Obtenha os valores")
         if botao: 
+            # Definições das funções
             def fx(x): 
                 f = (beta/eta)*((x/eta)**(beta-1))*np.exp(-(x/eta)**beta) 
                 return f 
@@ -51,183 +54,172 @@ def main():
             def Rh(h): 
                 return 1- Fh(h) 
 
+            def objetivo(y):
+                S, T, Z = y  # Corrigindo a desestruturação das variáveis
+                # CASO 1
+                def P1(S):
+                    return Fx(S)
+                def C1(S):
+                    return cf*P1(S)
+                def V1(S):
+                    return (quad(lambda x: x*fx(x), 0, S)[0])  
 
-        def objetivo(y):
-            S, T, Z = y  # Corrigindo a desestruturação das variáveis
+                # CASO 2
+                def P2(S,T):
+                    return Rh(T-S)*(Fx(T) - Fx(S)) + (dblquad(lambda x, h: fh(h)*fx(x), 0, T-S, S, lambda h: S+h)[0])
+                def C2(S,T):
+                    return cf*P2(S,T)
+                def V2(S,T):
+                    return Rh(T-S)*(quad(lambda x: x*fx(x), S, T)[0])+ (dblquad(lambda x, h: x*fh(h)*fx(x), 0, T-S, S, lambda h: S+h)[0])
+
+                # CASO 3
+                def P3(S,T,Z):
+                    return p*Rh(Z-S)*(Fx(Z)-Fx(T)) + p*(dblquad(lambda x, h: fh(h)*fx(x), T-S, Z-S, T, lambda h: h+S)[0])
+                def C3(S,T,Z):
+                    return cf*P3(S,T,Z)
+                def V3(S,T,Z):
+                    return  p*Rh(T-S)*(quad(lambda x: x*fx(x), T, Z)[0]) + p*(dblquad(lambda x, h: x*fh(h)*fx(x), T-S, Z-S, T, lambda h: h+S)[0])
+
+                # CASO 4
+                def P4(S,T):
+                    return (quad(lambda h: fh(h)*Rx(S+h), 0, T-S)[0])
+                def C4(S,T):
+                    return co*P4(S, T)
+                def V4(S,T):
+                    return (quad(lambda h: (S+h)*fh(h)*Rx(S+h), 0, T-S)[0])
+
+                # CASO 5
+                def P5(S,T,Z):
+                    return p*(quad(lambda h: fh(h)*Rx(S+h), T-S, Z-S)[0])
+                def C5(S,T,Z):
+                    return cw*P5(S, T, Z)
+                def V5(S,T,Z): 
+                    return p*(quad(lambda h: (S+h)*fh(h)*Rx(S+h), T-S, Z-S)[0])
+
+                # CASO 6
+                def P6(S,T):
+                    return (1-p)*Rh(T-S)*Rx(T) 
+                def C6(S,T):
+                    return cp*P6(S, T)
+                def V6(S,T):
+                    return T*P6(S, T)
+
+                # CASO 7 
+                def P7(S,T,Z):
+                    return p*Rh(Z-S)*Rx(Z)
+                def C7(S,T,Z):
+                    return cv*P7(S, T, Z)
+                def V7(S,T,Z):
+                    return Z*P7(S, T, Z)
+
+                SOMA_PROB=P1(S)+P2(S,T)+P3(S, T, Z)+P4(S, T) + P5(S, T, Z) + P6(S, T)+P7(S, T, Z)
+                SOMA_CUST=C1(S)+C2(S,T)+C3(S, T, Z)+C4(S, T) + C5(S, T, Z) + C6(S, T)+C7(S, T, Z)
+                SOMA_VIDA=V1(S)+V2(S,T)+V3(S, T, Z)+V4(S, T) + V5(S, T, Z) + V6(S, T)+V7(S, T, Z)
+
+                TAXA_CUSTO=SOMA_CUST/SOMA_VIDA
+                return TAXA_CUSTO
+
+            x0 = [0.9, 1.0, 2.0]
+
+            def cond1(y):
+                return y[1]-y[0] # T >= S
+
+            def cond2(y):
+                return y[2]-y[1] # Z >= T
+
+            c1 = {'type': 'ineq', 'fun': cond1}
+            c2 = {'type': 'ineq', 'fun': cond2}
+
+            cons = [c1, c2]
+
+            bx0 = [0.1, 50]
+            bx1 = [0.1, 50]
+            bx2 = [0.1, 50]
+
+            ret = minimize(objetivo, x0, method='SLSQP', bounds=[bx0, bx1, bx2], constraints=cons)
+            S, T, Z = ret.x[0], ret.x[1], ret.x[2]
+
+            st.write('S = :', S)
+            st.write('T = :', T)
+            st.write('Z = :', Z)
+            st.write('Taxa de custo = :', ret.fun)  # Corrigindo o nome da variável
+
+    # Função MTBOF anterior
+    def MTBOF(S,T,Z):
+        def P1(S):
+            return Fx(S)
+        def C1(S):
+            return cf*P1(S)
+        def V1(S):
+            return (quad(lambda x: x*fx(x), 0, S)[0])  
         
-            # CASO 1
-            def P1(S):
-                return Fx(S)
+        #CASO 2
+        def P2(S,T):
+            return Rh(T-S)*(Fx(T) - Fx(S)) + (dblquad(lambda x, h: fh(h)*fx(x), 0, T-S, S, lambda h: S+h)[0])
+        def C2(S,T):
+            return cf*P2(S,T)
+        def V2(S,T):
+            return Rh(T-S)*(quad(lambda x: x*fx(x), S, T)[0])+ (dblquad(lambda x, h: x*fh(h)*fx(x), 0, T-S, S, lambda h: S+h)[0])
         
-            def C1(S):
-                return cf * P1(S)
+        #CASO 3
+        def P3(S,T,Z):
+            return p*Rh(Z-S)*(Fx(Z)-Fx(T)) + p*(dblquad(lambda x, h: fh(h)*fx(x), T-S, Z-S, T, lambda h: h+S)[0])
+        def C3(S,T,Z):
+            return cf*P3(S,T,Z)
+        def V3(S,T,Z):
+            return  p*Rh(T-S)*(quad(lambda x: x*fx(x), T, Z)[0]) + p*(dblquad(lambda x, h: x*fh(h)*fx(x), T-S, Z-S, T, lambda h: h+S)[0])
         
-            def V1(S):
-                return quad(lambda x: x * fx(x), 0, S)[0]
+        #CASO 4
+        def P4(S,T):
+            return (quad(lambda h: fh(h)*Rx(S+h), 0, T-S)[0])
+        def C4(S,T):
+            return co*P4(S, T)
+        def V4(S,T):
+            return (quad(lambda h: (S+h)*fh(h)*Rx(S+h), 0, T-S)[0])
         
-            # CASO 2
-            def P2(S, T):
-                return Rh(T - S) * (Fx(T) - Fx(S)) + (dblquad(lambda x, h: fh(h) * fx(x), 0, T - S, S, lambda h: S + h)[0])
+        #CASO 5
+        def P5(S,T,Z):
+            return p*(quad(lambda h: fh(h)*Rx(S+h), T-S, Z-S)[0])
+        def C5(S,T,Z):
+            return cw*P5(S, T, Z)
+        def V5(S,T,Z): 
+            return p*(quad(lambda h: (S+h)*fh(h)*Rx(S+h), T-S, Z-S)[0])
         
-            def C2(S, T):
-                return cf * P2(S, T)
+        #CASO 6
+        def P6(S,T):
+            return (1-p)*Rh(T-S)*Rx(T) 
+        def C6(S,T):
+            return cp*P6(S, T)
+        def V6(S,T):
+            return T*P6(S, T)
         
-            def V2(S, T):
-                return Rh(T - S) * (quad(lambda x: x * fx(x), S, T)[0]) + (dblquad(lambda x, h: x * fh(h) * fx(x), 0, T - S, S, lambda h: S + h)[0])
+        #CASO 7 
+        def P7(S,T,Z):
+            return p*Rh(Z-S)*Rx(Z)
+        def C7(S,T,Z):
+            return cv*P7(S, T, Z)
+        def V7(S,T,Z):
+            return Z*P7(S, T, Z)
 
-     #CASO 3
-            def P3(S,T,Z):
-                return p*Rh(Z-S)*(Fx(Z)-Fx(T)) + p*(dblquad(lambda x, h: fh(h)*fx(x), T-S, Z-S, T, lambda h: h+S)[0])
-            def C3(S,T,Z):
-                return cf*P3(S,T,Z)
-            def V3(S,T,Z):
-                return  p*Rh(T-S)*(quad(lambda x: x*fx(x), T, Z)[0]) + p*(dblquad(lambda x, h: x*fh(h)*fx(x), T-S, Z-S, T, lambda h: h+S)[0])
-    
-    #CASO 4
-            def P4(S,T):
-                return (quad(lambda h: fh(h)*Rx(S+h), 0, T-S)[0])
-            def C4(S,T):
-                return co*P4(S, T)
-            def V4(S,T):
-                return (quad(lambda h: (S+h)*fh(h)*Rx(S+h), 0, T-S)[0])
-    
-    #CASO 5
-            def P5(S,T,Z):
-                return p*(quad(lambda h: fh(h)*Rx(S+h), T-S, Z-S)[0])
-            def C5(S,T,Z):
-                return cw*P5(S, T, Z)
-            def V5(S,T,Z): 
-                return p*(quad(lambda h: (S+h)*fh(h)*Rx(S+h), T-S, Z-S)[0])
-    
-    #CASO 6
-            def P6(S,T):
-                return (1-p)*Rh(T-S)*Rx(T) 
-            def C6(S,T):
-                return cp*P6(S, T)
-            def V6(S,T):
-                return T*P6(S, T)
-    
-    #CASO 7 
-            def P7(S,T,Z):
-                return p*Rh(Z-S)*Rx(Z)
-            def C7(S,T,Z):
-                return cv*P7(S, T, Z)
-            def V7(S,T,Z):
-                return Z*P7(S, T, Z)
+        SOMA_PROB_FALHAS = P1(S) + P2(S, T) + P3(S, T, Z)
+        SOMA_VIDA = V1(S) + V2(S, T) + V3(S, T, Z) + V4(S, T) + V5(S, T, Z) + V6(S, T) + V7(S, T, Z)
 
-            SOMA_PROB = P1(S) + P2(S, T) + P3(S, T, Z) + P4(S, T) + P5(S, T, Z) + P6(S, T) + P7(S, T, Z)
-            SOMA_CUST = C1(S) + C2(S, T) + C3(S, T, Z) + C4(S, T) + C5(S, T, Z) + C6(S, T) + C7(S, T, Z)
-            SOMA_VIDA = V1(S) + V2(S, T) + V3(S, T, Z) + V4(S, T) + V5(S, T, Z) + V6(S, T) + V7(S, T, Z)
+        MTBOF = SOMA_PROB_FALHAS / SOMA_VIDA
+        return MTBOF
+        pass
 
-            TAXA_CUSTO = SOMA_CUST / SOMA_VIDA
-            return TAXA_CUSTO
+    st.write('MTBOF:', MTBOF(S,T,Z))
 
-# Defina as variáveis iniciais
-x0 = [0.9, 1.0, 2.0]
+    if choice == menu[1]:
+        st.header(menu[1])
+        st.write('''Fazer o texto para colocar aqui''')
 
-def cond1(y):
-    return y[1] - y[0]  # T >= S
+    if choice == menu[2]:
+        st.header(menu[2])
+        st.write('''The Research Group on Risk and Decision Analysis in Operations and Maintenance was created in 2012 
+        in order to bring together different researchers who work in the following areas: risk, maintenance and 
+        operation modelling. Learn more about it through our website.''')
+        st.markdown('[Click here to be redirected to our website](http://random.org.br/en/)', False)
 
-def cond2(y):
-    return y[2] - y[1]  # Z >= T
-
-c1 = {'type': 'ineq', 'fun': cond1}
-c2 = {'type': 'ineq', 'fun': cond2}
-
-cons = [c1, c2]
-
-bx0=[0.1,50]
-bx1=[0.1,50]
-bx2=[0.1,50]
-
-ret = minimize(objetivo, x0, method='SLSQP', bounds=[bx0, bx1, bx2], constraints=cons)
-S, T, Z = ret.x[0], ret.x[1], ret.x[2]
-    
-st.write('S = :', S)
-st.write('T = :', T)
-st.write('Z = :', Z)
-st.write('Taxa de custo = :', ret.fun)  # Corrigindo o nome da variável
-  
-
-
-def MTBOF(S, T, Z):
-    def P1(S):
-        return Fx(S)
-    def C1(S):
-        return cf*P1(S)
-    def V1(S):
-        return (quad(lambda x: x*fx(x), 0, S)[0])  
-    
-    #CASO 2
-    def P2(S,T):
-        return Rh(T-S)*(Fx(T) - Fx(S)) + (dblquad(lambda x, h: fh(h)*fx(x), 0, T-S, S, lambda h: S+h)[0])
-    def C2(S,T):
-        return cf*P2(S,T)
-    def V2(S,T):
-        return Rh(T-S)*(quad(lambda x: x*fx(x), S, T)[0])+ (dblquad(lambda x, h: x*fh(h)*fx(x), 0, T-S, S, lambda h: S+h)[0])
-    
-    #CASO 3
-    def P3(S,T,Z):
-        return p*Rh(Z-S)*(Fx(Z)-Fx(T)) + p*(dblquad(lambda x, h: fh(h)*fx(x), T-S, Z-S, T, lambda h: h+S)[0])
-    def C3(S,T,Z):
-        return cf*P3(S,T,Z)
-    def V3(S,T,Z):
-        return  p*Rh(T-S)*(quad(lambda x: x*fx(x), T, Z)[0]) + p*(dblquad(lambda x, h: x*fh(h)*fx(x), T-S, Z-S, T, lambda h: h+S)[0])
-    
-    #CASO 4
-    def P4(S,T):
-        return (quad(lambda h: fh(h)*Rx(S+h), 0, T-S)[0])
-    def C4(S,T):
-        return co*P4(S, T)
-    def V4(S,T):
-        return (quad(lambda h: (S+h)*fh(h)*Rx(S+h), 0, T-S)[0])
-    
-    #CASO 5
-    def P5(S,T,Z):
-        return p*(quad(lambda h: fh(h)*Rx(S+h), T-S, Z-S)[0])
-    def C5(S,T,Z):
-        return cw*P5(S, T, Z)
-    def V5(S,T,Z): 
-        return p*(quad(lambda h: (S+h)*fh(h)*Rx(S+h), T-S, Z-S)[0])
-    
-    #CASO 6
-    def P6(S,T):
-        return (1-p)*Rh(T-S)*Rx(T) 
-    def C6(S,T):
-        return cp*P6(S, T)
-    def V6(S,T):
-        return T*P6(S, T)
-    
-    #CASO 7 
-    def P7(S,T,Z):
-        return p*Rh(Z-S)*Rx(Z)
-    def C7(S,T,Z):
-        return cv*P7(S, T, Z)
-    def V7(S,T,Z):
-        return Z*P7(S, T, Z)
-
-    SOMA_PROB_FALHAS = P1(S) + P2(S, T) + P3(S, T, Z)
-    SOMA_VIDA = V1(S) + V2(S, T) + V3(S, T, Z) + V4(S, T) + V5(S, T, Z) + V6(S, T) + V7(S, T, Z)
-
-    MTBOF = SOMA_PROB_FALHAS / SOMA_VIDA
-    return MTBOF
-
-st.write('MTBOF:', MTBOF(S, T, Z))
-if choice == menu[1]:
-    st.header(menu[1])
-    st.write('''Fazer o texto para colocar aqui''')
-    
-if choice == menu[2]:
-    st.header(menu[2])
-    st.write('''The Research Group on Risk and Decision Analysis in Operations and Maintenance was created in 2012 
-    in order to bring together different researchers who work in the following areas: risk, maintenance and 
-    operation modelling. Learn more about it through our website.''')
-    st.markdown('[Click here to be redirected to our website](http://random.org.br/en/)', False)
-
-if st._is_running_with_streamlit:
-    main()
-else:
-    sys.argv = ["streamlit", "run", sys.argv[0]]
-    sys.exit(stcli.main())
 if __name__ == "__main__":
     main()
