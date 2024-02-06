b730ff913530c9572716cb7ecff272503e19ce5d
Renn444 <128076981+Renn444@users.noreply.github.com>
2024-02-06 17:33:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -268,8 +268,35 @@ def main():
         S = st.number_input('Valor de S')
         T= st.number_input('Valor de T')    
         Z= st.number_input('Valor de Z')
-         #y = (s, t, z)
-    
+        y = (S, T, Z)
+        
+        st.subheader("Clique no botão abaixo para rodar esse aplicativo:")
+
+        botao = st.button("Obtenha os valores")
+        if botao: 
+        Lista_test= []
+        for i in range(0, 400):
+            beta = random.uniform(Beta * (1 - betaimprec), Beta * (1 + betaimprec))
+            eta = random.uniform(Eta * (1 - etaimprec), Eta * (1 + etaimprec))
+            lbda = random.uniform(Lbda * (1 - lbdaimprec), Lbda * (1 + lbdaimprec))
+            cp = random.uniform(Cp * (1 - cpimprec), Cp * (1 + cpimprec))
+            cv = random.uniform(Cv * (1 - cvimprec), Cv * (1 + cvimprec))
+            co = random.uniform(Co * (1 - coimprec), Co * (1 + coimprec))
+            cf = random.uniform(Cf * (1 - cfimprec), Cf * (1 + cfimprec))
+            cw = random.uniform(Cw * (1 - cwimprec), Cw * (1 + cwimprec))
+            p = random.uniform(P * (1 - etaimprec), P * (1 + etaimprec))
+        
+            cr = objetivo(y)
+            Lista_test.append(cr)
+            st.write("Média da Taxa de Custo:", sum(Lista_test) / len(Lista_test))
+            st.write('Desvio Padrão da Taxa de Custo:', np.std(Lista_test))
+            
+               # Criar box-plot
+            st.boxplot(Lista_test)
+            st.title('Box-Plot da Taxa de Custo')
+            st.ylabel('Taxa de Custo')
+            st.show()
+            
     if choice == menu[2]:
         st.header(menu[2])
         st.write('''The Research Group on Risk and Decision Analysis in Operations and Maintenance was created in 2012 
