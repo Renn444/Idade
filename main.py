0dc456c916c8b430450c72579a6666622c32c8de
Renn444 <128076981+Renn444@users.noreply.github.com>
2024-02-07 13:10:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import streamlit as st
 import sys
 from streamlit import cli as stcli
 from PIL import Image
-import random  # Add this line to import the 'random' module
-
 
 # Funções e definições anteriores
 
@@ -270,129 +268,8 @@ def main():
         S = st.number_input('Valor de S')
         T= st.number_input('Valor de T')    
         Z= st.number_input('Valor de Z')
-        y = (S, T, Z)
-        
-        st.subheader("Clique no botão abaixo para rodar esse aplicativo:")
-
-        botao = st.button("Obtenha os valores")
-        if botao:
-            resultados = []
-            Lista_test= []#começa aqui
-            def fx(x):
-               f = (beta/eta)*((x/eta)**(beta-1))*np.exp(-(x/eta)**beta)
-               return f
-            def Fx(x):
-                return 1 - np.exp(-(x/eta)**beta) 
-            def Rx(x): 
-                return 1 - Fx(x)
-                
-            # parte que não entendi muito bem 
-            # tempo entre oportunidades
-            def fh(h):
-               return lbda*np.exp(-(lbda*h))
-            def Fh(h):
-               return 1 - np.exp(-(lbda*h))
-            def Rh(h):
-                return 1- Fh(h) 
-            
-            def objetivo(y):
-                global eta, beta, lbda, p, co, cp, cv, cw, cf
-                
-                S=y[0]
-                T=y[1]
-                Z=y[2]
-                #CASO 1
-                def P1(S):
-                    return Fx(S)
-                def C1(S):
-                    return cf*P1(S)
-                def V1(S):
-                    return (quad(lambda x: x*fx(x), 0, S)[0])  
-                
-                #CASO 2
-                def P2(S,T):
-                    return Rh(T-S)*(Fx(T) - Fx(S)) + (dblquad(lambda x, h: fh(h)*fx(x), 0, T-S, S, lambda h: S+h)[0])
-                def C2(S,T):
-                    return cf*P2(S,T)
-                def V2(S,T):
-                    return Rh(T-S)*(quad(lambda x: x*fx(x), S, T)[0])+ (dblquad(lambda x, h: x*fh(h)*fx(x), 0, T-S, S, lambda h: S+h)[0])
-                
-                #CASO 3
-                def P3(S,T,Z):
-                    return p*Rh(Z-S)*(Fx(Z)-Fx(T)) + p*(dblquad(lambda x, h: fh(h)*fx(x), T-S, Z-S, T, lambda h: h+S)[0])
-                def C3(S,T,Z):
-                    return cf*P3(S,T,Z)
-                def V3(S,T,Z):
-                    return  p*Rh(T-S)*(quad(lambda x: x*fx(x), T, Z)[0]) + p*(dblquad(lambda x, h: x*fh(h)*fx(x), T-S, Z-S, T, lambda h: h+S)[0])
-                
-                #CASO 4
-                def P4(S,T):
-                    return (quad(lambda h: fh(h)*Rx(S+h), 0, T-S)[0])
-                def C4(S,T):
-                    return co*P4(S, T)
-                def V4(S,T):
-                    return (quad(lambda h: (S+h)*fh(h)*Rx(S+h), 0, T-S)[0])
-                
-                #CASO 5
-                def P5(S,T,Z):
-                    return p*(quad(lambda h: fh(h)*Rx(S+h), T-S, Z-S)[0])
-                def C5(S,T,Z):
-                    return cw*P5(S, T, Z)
-                def V5(S,T,Z): 
-                    return p*(quad(lambda h: (S+h)*fh(h)*Rx(S+h), T-S, Z-S)[0])
-                
-                #CASO 6
-                def P6(S,T):
-                    return (1-p)*Rh(T-S)*Rx(T) 
-                def C6(S,T):
-                    return cp*P6(S, T)
-                def V6(S,T):
-                    return T*P6(S, T)
-                
-                #CASO 7 
-                def P7(S,T,Z):
-                    return p*Rh(Z-S)*Rx(Z)
-                def C7(S,T,Z):
-                    return cv*P7(S, T, Z)
-                def V7(S,T,Z):
-                    return Z*P7(S, T, Z)
-                
-                # SOMA_PROB=P1(S)+P2(S,T)+P3(S, T, Z)+P4(S, T) + P5(S, T, Z) + P6(S, T)+P7(S, T, Z)
-                SOMA_CUST=C1(S)+C2(S,T)+C3(S, T, Z)+C4(S, T) + C5(S, T, Z) + C6(S, T)+C7(S, T, Z)
-                SOMA_VIDA=V1(S)+V2(S,T)+V3(S, T, Z)+V4(S, T) + V5(S, T, Z) + V6(S, T)+V7(S, T, Z)
-                
-                TAXA_CUSTO=SOMA_CUST/SOMA_VIDA
-                return TAXA_CUSTO
-            x0 = [0.9, 1.0,2.0]
-            
-            def cond1(y):
-                return y[1]-y[0] #T>=S
-            
-            def cond2(y):
-                return y[2]-y[1] #Z>=T  
-            # termina aqui
-            for i in range(0, 400):
-                beta = random.uniform(Beta * (1 - betaimprec), Beta * (1 + betaimprec))
-                eta = random.uniform(Eta * (1 - etaimprec), Eta * (1 + etaimprec))
-                lbda = random.uniform(Lbda * (1 - lbdaimprec), Lbda * (1 + lbdaimprec))
-                cp = random.uniform(Cp * (1 - cpimprec), Cp * (1 + cpimprec))
-                cv = random.uniform(Cv * (1 - cvimprec), Cv * (1 + cvimprec))
-                co = random.uniform(Co * (1 - coimprec), Co * (1 + coimprec))
-                cf = random.uniform(Cf * (1 - cfimprec), Cf * (1 + cfimprec))
-                cw = random.uniform(Cw * (1 - cwimprec), Cw * (1 + cwimprec))
-                p = random.uniform(P * (1 - etaimprec), P * (1 + etaimprec))
-            
-                cr = objetivo(y)
-                Lista_test.append(cr)
-            print("Média :", sum(Lista_test)/len(Lista_test))
-            print('Devios Padrão:', np.std(Lista_test))
-            
-            # Criar box-plot
-            plt.boxplot(Lista_test)
-            plt.title('Box-Plot da Taxa de Custo')
-            plt.ylabel('Taxa de Custo')
-            plt.show()
-
+         #y = (s, t, z)
+    
     if choice == menu[2]:
         st.header(menu[2])
         st.write('''The Research Group on Risk and Decision Analysis in Operations and Maintenance was created in 2012 
