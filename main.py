9801cf23bf8a8404aef859b65d914d587b7a548e
Renn444 <128076981+Renn444@users.noreply.github.com>
2024-03-30 16:17:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import streamlit as st
 import sys
 from streamlit import cli as stcli
 from PIL import Image
-import random 
 
 # Funções e definições anteriores
 
@@ -25,17 +24,15 @@ def main():
         st.header(menu[0])
         st.subheader("Insira os valores dos parâmetros abaixo:")
         
-        #beta = st.number_input('Parâmetro de forma (beta)', value=2.0, step=0.1, format='%.1f')
-        beta = st.number_input('Parâmetro de forma (beta)')
-        eta = st.number_input('Parâmetro de escala (eta)')    
-        lbda = st.number_input('Taxa de Chegada de Oportunidade (Lambda)')
-        cp = st.number_input('Custo de Substituição Preventiva em T(programado):') 
-        cv = st.number_input('Custo de Substituição Preventiva em Z:')
-        co = st.number_input('Custo de Substituição Preventiva em Oportunidade:') 
-        cf = st.number_input('Custo da manutenção corretiva:') 
-        cw = st.number_input('Substituição oportuna entre T e Z:')
-        p = st.number_input('#Probabilidade de Impedimento:')
-
+        beta = st.number_input('Parâmetro de forma (beta)', value=np.random.uniform(2, 5), step=0.1, format='%.1f')
+        eta = st.number_input('Parâmetro de escala (eta)', value=np.random.uniform(3, 4), step=0.1, format='%.1f')
+        lbda = st.number_input('Taxa de Chegada de Oportunidade (Lambda)', value=np.random.uniform(0, 4), step=0.1, format='%.1f')
+        cp = st.number_input('Custo de Substituição Preventiva em T(programado):', value=1.0, step=10.0, format='%.1f') #FEITO
+        cv = st.number_input('Custo de Substituição Preventiva em Z:', value=np.random.uniform(1, 3), step=10.0, format='%.1f')
+        co = st.number_input('Custo de Substituição Preventiva em Oportunidade:', value=np.random.uniform(0.25, 1), step=10.0, format='%.1f') #FEITO
+        cf = st.number_input('Custo da manutenção corretiva:', min_value=0.01, step=0.01, value=np.random.uniform(2.5, 10)) #FEITO
+        cw = st.number_input('substituição oportuna entre T e Z:', value=np.random.uniform(0.25, 1), step=10.0, format='%.1f')
+        p = st.number_input('#Probabilidade de Impedimento:', min_value=0.01, step=0.01, value=np.random.uniform(0, 0.2))
         
         st.subheader("Clique no botão abaixo para rodar esse aplicativo:")
         
@@ -70,7 +67,7 @@ def main():
 
                 # CASO 2
                 def P2(S,T):
-                    return Rh(T-S)*(Fx(T) - Fx(S)) + (dblquad(lambda x, h: fh(h)*fx(x), 0, T-S, lambda x: S, lambda x: S+h)[0])
+                    return Rh(T-S)*(Fx(T) - Fx(S)) + (dblquad(lambda x, h: fh(h)*fx(x), 0, T-S, S, lambda h: S+h)[0])
                 def C2(S,T):
                     return cf*P2(S,T)
                 def V2(S,T):
@@ -116,7 +113,7 @@ def main():
                 def V7(S,T,Z):
                     return Z*P7(S, T, Z)
 
-                #SOMA_PROB=P1(S)+P2(S,T)+P3(S, T, Z)+P4(S, T) + P5(S, T, Z) + P6(S, T)+P7(S, T, Z)
+                SOMA_PROB=P1(S)+P2(S,T)+P3(S, T, Z)+P4(S, T) + P5(S, T, Z) + P6(S, T)+P7(S, T, Z)
                 SOMA_CUST=C1(S)+C2(S,T)+C3(S, T, Z)+C4(S, T) + C5(S, T, Z) + C6(S, T)+C7(S, T, Z)
                 SOMA_VIDA=V1(S)+V2(S,T)+V3(S, T, Z)+V4(S, T) + V5(S, T, Z) + V6(S, T)+V7(S, T, Z)
 
@@ -208,11 +205,11 @@ def main():
                 SOMA_PROB_FALHAS = P1(S) + P2(S, T) + P3(S, T, Z)
                 SOMA_VIDA = V1(S) + V2(S, T) + V3(S, T, Z) + V4(S, T) + V5(S, T, Z) + V6(S, T) + V7(S, T, Z)
 
-                MTBOF = SOMA_VIDA / SOMA_PROB_FALHAS
+                MTBOF = SOMA_PROB_FALHAS / SOMA_VIDA
                 return MTBOF
             pass
 
-            st.write('Tempo médio entre falhas operacionais:', MTBOF(S,T,Z))
+            st.write('MTBOF:', MTBOF(S,T,Z))
             CR=ret.fun
             resultado_iteracao = {  # Crie um dicionário para armazenar os resultados de uma iteração
                 'eta': eta,
@@ -233,168 +230,13 @@ def main():
             resultados_df = pd.DataFrame(resultados)
             media = resultados_df['taxa'].mean()
             desvio_padrao = resultados_df['taxa'].std()
-            resultados.append(resultado_iteracao)
-            resultados_df = pd.DataFrame(resultados)
-            media = resultados_df['taxa'].mean()
-            desvio_padrao = resultados_df['taxa'].std()
-
-            if np.isnan(desvio_padrao):
-                st.write("Não há variabilidade nos dados para calcular o desvio padrão.")
-            else:
-                st.write(f'Média da Taxa de Custo: {media}')
-                st.write(f'Desvio Padrão da Taxa de Custo: {desvio_padrao}')
+            st.write(f'Média da Taxa de Custo: {media}')
+            st.write(f'Desvio Padrão da Taxa de Custo: {desvio_padrao}')
 
     if choice == menu[1]:
         st.header(menu[1])
         st.write('''Fazer o texto para colocar aqui''')
-        Beta = st.number_input('Parâmetro de forma (beta)')
-        Eta = st.number_input('Parâmetro de escala (eta)')    
-        Lbda = st.number_input('Taxa de Chegada de Oportunidade (Lambda)')
-        Cp = st.number_input('Custo de Substituição Preventiva em T(programado):') 
-        Cv = st.number_input('Custo de Substituição Preventiva em Z:')
-        Co = st.number_input('Custo de Substituição Preventiva em Oportunidade:') 
-        Cf = st.number_input('Custo da manutenção corretiva:') 
-        Cw = st.number_input('Substituição oportuna entre T e Z:')
-        P = st.number_input('Probabilidade de Impedimento:')
-         
-        betaimprec = st.number_input('Imprecisão do Parâmetro de forma (beta)')
-        etaimprec = st.number_input('Imprecisão do Parâmetro de escala (eta)')    
-        lbdaimprec = st.number_input('Imprecisão da Taxa de Chegada de Oportunidade (Lambda)')
-        cpimprec = st.number_input('Imprecisão do Custo de Substituição Preventiva em T(programado):') 
-        cvimprec = st.number_input('Imprecisão do Custo de Substituição Preventiva em Z:')
-        coimprec = st.number_input('Imprecisão do Custo de Substituição Preventiva em Oportunidade:') 
-        cfimprec = st.number_input('Imprecisão do Custo da manutenção corretiva:') 
-        cwimprec = st.number_input('Imprecisão da Substituição oportuna entre T e Z:')
-        pimpre = st.number_input('Imprecisão da Probabilidade de Impedimento:')
-        S = st.number_input('Valor de S')
-        T= st.number_input('Valor de T')    
-        Z= st.number_input('Valor de Z')
-        y = (S, T, Z)
-        
-        st.subheader("Clique no botão abaixo para rodar esse aplicativo:")
 
-        botao = st.button("Obtenha os valores")
-        if botao:
-            resultados = []
-            Lista_test= []
-            def fx(x):
-                f = (beta/eta)*((x/eta)**(beta-1))*np.exp(-(x/eta)**beta) 
-                return f 
-            def Fx(x):
-                return 1 - np.exp(-(x/eta)**beta) 
-            def Rx(x): 
-                return 1 - Fx(x)
-                    
-            def fh(h):
-                return lbda*np.exp(-(lbda*h))
-            def Fh(h):
-                return 1 - np.exp(-(lbda*h)) 
-            def Rh(h): 
-                return 1- Fh(h) 
-
-            def objetivo(y):
-                S, T, Z = y  # Corrigindo a desestruturação das variáveis
-                # CASO 1
-                def P1(S):
-                    return Fx(S)
-                #P1 = 0.2
-                def C1(S):
-                    return Cf*P1(S)
-                    #return cf*P1
-                def V1(S):
-                    return (quad(lambda x: x*fx(x), 0, S)[0])  
-
-                # CASO 2
-                def P2(S,T):
-                    return Rh(T-S)*(Fx(T) - Fx(S)) + (dblquad(lambda x, h: fh(h)*fx(x), 0, T-S, S, lambda h: S+h)[0])
-                def C2(S,T):
-                    return Cf*P2(S,T)
-                def V2(S,T):
-                    return Rh(T-S)*(quad(lambda x: x*fx(x), S, T)[0])+ (dblquad(lambda x, h: x*fh(h)*fx(x), 0, T-S, S, lambda h: S+h)[0])
-
-                # CASO 3
-                def P3(S,T,Z):
-                    #return p*Rh(Z-S)*(Fx(Z)-Fx(T)) + p*(dblquad(lambda x, h: fh(h)*fx(x), T-S, Z-S, T, lambda h: h+S)[0])
-                    return p*Rh(Z-S)*(Fx(Z)-Fx(T)) + p*(dblquad(lambda x, h: fh(h)*fx(x), T-S, Z-S, lambda x: T, lambda x: h+S)[0])
-                def C3(S,T,Z):
-                    return Cf*P3(S,T,Z)
-                def V3(S,T,Z):
-                    return  p*Rh(T-S)*(quad(lambda x: x*fx(x), T, Z)[0]) + p*(dblquad(lambda x, h: x*fh(h)*fx(x), T-S, Z-S, T, lambda h: h+S)[0])
-
-                # CASO 4
-                def P4(S,T):
-                    return (quad(lambda h: fh(h)*Rx(S+h), 0, T-S)[0])
-                def C4(S,T):
-                    return Co*P4(S, T)
-                def V4(S,T):
-                    return (quad(lambda h: (S+h)*fh(h)*Rx(S+h), 0, T-S)[0])
-
-                # CASO 5
-                def P5(S,T,Z):
-                    return p*(quad(lambda h: fh(h)*Rx(S+h), T-S, Z-S)[0])
-                def C5(S,T,Z):
-                    return Cw*P5(S, T, Z)
-                def V5(S,T,Z): 
-                    return p*(quad(lambda h: (S+h)*fh(h)*Rx(S+h), T-S, Z-S)[0])
-
-                # CASO 6
-                def P6(S,T):
-                    return (1-p)*Rh(T-S)*Rx(T) 
-                def C6(S,T):
-                    return Cp*P6(S, T)
-                def V6(S,T):
-                    return T*P6(S, T)
-
-                # CASO 7 
-                def P7(S,T,Z):
-                    return p*Rh(Z-S)*Rx(Z)
-                def C7(S,T,Z):
-                    return Cv*P7(S, T, Z)
-                def V7(S,T,Z):
-                    return Z*P7(S, T, Z)
-
-                #SOMA_PROB=P1(S)+P2(S,T)+P3(S, T, Z)+P4(S, T) + P5(S, T, Z) + P6(S, T)+P7(S, T, Z)
-                SOMA_CUST=C1(S)+C2(S,T)+C3(S, T, Z)+C4(S, T) + C5(S, T, Z) + C6(S, T)+C7(S, T, Z)
-                SOMA_VIDA=V1(S)+V2(S,T)+V3(S, T, Z)+V4(S, T) + V5(S, T, Z) + V6(S, T)+V7(S, T, Z)
-
-                TAXA_CUSTO=SOMA_CUST/SOMA_VIDA
-                return TAXA_CUSTO 
-
-            x0 = [0.9, 1.0,2.0]
-            
-            def cond1(y):
-                return y[1]-y[0] #T>=S
-            
-            def cond2(y):
-                return y[2]-y[1] #Z>=T
-                
-            for i in range(0, 400):
-                beta = random.uniform(Beta * (1 - betaimprec), Beta * (1 + betaimprec))
-                eta = random.uniform(Eta * (1 - etaimprec), Eta * (1 + etaimprec))
-                lbda = random.uniform(Lbda * (1 - lbdaimprec), Lbda * (1 + lbdaimprec))
-                cp = random.uniform(Cp * (1 - cpimprec), Cp * (1 + cpimprec))
-                cv = random.uniform(Cv * (1 - cvimprec), Cv * (1 + cvimprec))
-                co = random.uniform(Co * (1 - coimprec), Co * (1 + coimprec))
-                cf = random.uniform(Cf * (1 - cfimprec), Cf * (1 + cfimprec))
-                cw = random.uniform(Cw * (1 - cwimprec), Cw * (1 + cwimprec))
-                p = random.uniform(P * (1 - etaimprec), P * (1 + etaimprec))
-                cr = objetivo(y)
-                Lista_test.append(cr)
-                 # Exibir média e desvio padrão
-            st.write("Média:", sum(Lista_test) / len(Lista_test))
-            st.write('Desvio Padrão:', np.std(Lista_test))
-            
-            # Criar box-plot
-            fig, ax = plt.subplots()
-            ax.boxplot(Lista_test)
-            ax.set_title('Box-Plot da Taxa de Custo')
-            ax.set_ylabel('Taxa de Custo')
-            
-            # Exibir o gráfico no Streamlit
-            st.pyplot(fig)
-            print("Média :", sum(Lista_test)/len(Lista_test))
-            print('Devios Padrão:', np.std(Lista_test))
-            
     if choice == menu[2]:
         st.header(menu[2])
         st.write('''The Research Group on Risk and Decision Analysis in Operations and Maintenance was created in 2012 
