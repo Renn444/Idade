776d7b38b603dc20ff3cd3f51cdb0436987a429d
Renn444 <128076981+Renn444@users.noreply.github.com>
2024-04-12 14:07:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -33,8 +33,6 @@ def main():
         DT_max = st.number_input('Downtime máximo aceito:', min_value=0.01, step=0.01)
         st.subheader("Clique no botão abaixo para rodar esse aplicativo:")
         
-        st.subheader("Clique no botão abaixo para rodar esse aplicativo:")
-        
         botao = st.button("Obtenha os valores")
         if botao:
             def otm():
